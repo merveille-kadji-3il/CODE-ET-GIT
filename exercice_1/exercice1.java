cbc1f1359a5529ce94d38dfde362f9132e0f297c
Merveille <kadjiyom@3il.fr>
2024-11-26 20:10:21 +0100

    Ajout du fichier correspondant à l'exercice 1

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,23 +1,48 @@
 public class FactureManager {
+    //fonction pour la reduction alimentaire
+    public void reduc_alimentaire(){
+        total -= total * 0.05; // Réduction de 5%
+    }
 
-    // Calcul le total d'une facture
-    public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
+    // fonction pour la réduction électronique
+    public void reduc_electronique(){
+        total -= total * 0.1; // Réduction de 10%
+    }
+
+    // fonction pour la reduction de luxe
+    public void reduc_luxe(){
+        total -= total * 0.15; // Réduction de 15%
+    }
+
+        // Calcul le total d'une facture
+        /**
+         * @param typeProduit le type du produit (alimentaire, électronique, luxe)
+         * @param quantite la quantité de produit
+         * @param prixUnitaire le prix unitaire du produit
+         */
+    public double calculerFacture(String typeProduit, int quantite, double prixUnitaire){
         double total = quantite * prixUnitaire;
-
-        // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
+        if (total > 1000){
+                total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
         }
-
-        // Reduction sur le total
-        if (total > 1000) {
-            total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
-        }
-
         return total;
+    }
+
+    /**
+     * retourne le pourcentage de réduction applicable selon le type de produit
+     */
+
+    public double obtenirReduction(){
+            switch (typeProduit) {
+                case "Alimentaire" :
+                    calculerFacture.reduc_alimentaire();
+                    break;
+                case "Electronique" :
+                    calculerFacture.reduc_electronique();
+                    break;
+                case "Luxe"
+                    calculerFacture.reduc_luxe();
+                    break;
+            };
     }
 }
